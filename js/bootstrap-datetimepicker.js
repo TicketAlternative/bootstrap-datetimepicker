be70028877ac62918387c2e391fbf0b324e44d59
Patrick Arnett <patrick.a.arnett@gmail.com>
2020-06-12 14:17:24 -0400

    Use today's date when only time is present.

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -669,7 +669,8 @@
 
       if (typeof date === "string") {
         if (new RegExp(this.hour_minute).test(date) || new RegExp(this.hour_minute + ":[0-5][0-9]").test(date)) {
-          date = this.getDate()
+          today = new Date();
+          date = new Date(`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()} ${date}`);
         }
       }
 
